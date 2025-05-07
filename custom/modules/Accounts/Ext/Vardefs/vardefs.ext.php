0f633e5696adacc6a43b02a8d9620ebb854a46c6
Mihail <m.a.mokruschin@yandex.ru>
2025-05-07 16:14:15 +0500

    Added custom/modules and custom/include initial

---
+++ custom/modules/Accounts/Ext/Vardefs/vardefs.ext.php
@@ -0,0 +1,45 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+/** @var array $dictionary  */
+
+$dictionary['Account']['fields']['source'] = [
+    'name' => 'source',
+    'vname' => 'LBL_SOURCE',
+    'type' => 'enum',
+    'required' => true,
+    'audited' => true,
+    'comment' => 'Record source',
+    'options' => 'account_source_options',
+];
+
+$dictionary['Account']['fields']['source_another'] = [
+    'name' => 'source_another',
+    'vname' => 'LBL_SOURCE_ANOTHER',
+    'type' => 'text',
+    'required' => false,
+    'audited' => true,
+    'comment' => 'Record source comment',
+];
+
+ // created: 2025-04-28 07:46:09
+$dictionary['Account']['fields']['jjwg_maps_lng_c']['inline_edit']=1;
+
+ 
+
+ // created: 2025-04-28 07:46:09
+$dictionary['Account']['fields']['jjwg_maps_lat_c']['inline_edit']=1;
+
+ 
+
+ // created: 2025-04-28 07:46:09
+$dictionary['Account']['fields']['jjwg_maps_geocode_status_c']['inline_edit']=1;
+
+ 
+
+ // created: 2025-04-28 07:46:09
+$dictionary['Account']['fields']['jjwg_maps_address_c']['inline_edit']=1;
+
+ 
+?>
